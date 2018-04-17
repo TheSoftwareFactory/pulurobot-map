c98ea2a11591fa1aca812c8b4350a10c32ac287d
heedVox <heedvox@gmail.com>
2018-04-17 19:08:02 +0300

    Implemented db actions & W.I.P for WebSocket

--- express-server/app.js
+++ express-server/app.js
@@ -1,15 +1,21 @@
+const Sqlite = require('sqlite-pool');
+const Promise = require('bluebird');
+const port = process.env.port || 3000;
+const db = new Sqlite('../artifacts/maps.db', {Promise});
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(':memory:')
+var http = require('http');
+var WebSocketServer = require('websocket').server;
+var WebSocket = require('ws')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var server = require('./bin/www');
 var app = express();
 
 // view engine setup
@@ -42,6 +48,96 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+class Map{
+
+  constructor(points, table_name){
+    this.points = points;
+    this.table_name = table_name;
+  }
+
+  getPoints(){
+    return this.points;
+  }
+
+  getTableName(){
+    return this.table_name;
+  }
+
+  setPoints(x, y, free){
+    this.points.insert((x, y), free);
+
+    let sql = 'INSERT INTO '+this.table_name+' (x, y, status) VALUES (?, ?, ?)';
+    db.run(sql, [x, y, free], function(err){
+      if (err){
+        return console.error(err.message);
+      }
+      else{
+        return console.log('Row added successfully.')
+      }
+    });
+  }
+
+  getFromDB(map_name){
+    return Promise.all([
+      db.all('SELECT x, y, status FROM '+map_name)
+    ]).then(
+      (map)=>{
+        if(map===undefined){
+          return console.error('Error : result is undefined.');
+        }
+        else{
+          let x = map.rowid(0);
+          let y = map.rowid(1);
+          let free = map.rowid(2);
+          this.points.insert((x,y), free);
+        }
+      }
+    )
+  }
+
+}
+
+var server = http.createServer(app);
+
+var wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+
+
 
 /*
 
